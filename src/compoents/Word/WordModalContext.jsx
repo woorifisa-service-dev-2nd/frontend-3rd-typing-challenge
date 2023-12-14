bd9c6e235b0bdd60a022ba9cd78a8e8fb044977c
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2023-12-14 10:26:44 +0900

    add Modal

--- src/compoents/Word/WordModalContext.jsx
+++ src/compoents/Word/WordModalContext.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const WordModalContext = () => {
-  return <div>WordModalContext</div>;
+const WordModalContext = ({ onClose }) => {
+  
+  const playAgainHandler = () => {
+    // onPlayAgain(); // 게임 리셋되는 함수
+    onClose();
+
+  }
+
+  return (
+    <>
+      <h3>Challenge Completed!</h3>
+      <h4>Typing Callenge 게임을 다시 하시겠습니까?</h4>
+      <button onClick={playAgainHandler}>네! 또 할래요!</button>
+      <button onClick={onClose}>아니오. 안 할래요...</button>
+    </>
+  )
 };
 
 export default WordModalContext;
--- src/compoents/ui/Modal.jsx
+++ src/compoents/ui/Modal.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ children }) => {
+  return (
+    <>
+      <div data-cy="modal-backdrop" className='fixed top-0 left-0 w-full h-full backdrop-blur-md z-1'
+        onClick={onClose}>
+
+      </div>
+      <div>
+        {children}
+      </div>
+
+    </>
+  )
 };
 
 export default Modal;
