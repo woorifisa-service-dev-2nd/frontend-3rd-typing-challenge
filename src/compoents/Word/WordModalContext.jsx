fbeba8f861b51f5728c39076f4356e326820afb1
KyuliLee <151590254+KyuliLee@users.noreply.github.com>
2023-12-14 15:06:10 +0900

    Modal기능

--- src/compoents/Word/WordModalContext.jsx
+++ src/compoents/Word/WordModalContext.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WordModalContext = ({ onClose }) => {
-  
+
   const playAgainHandler = () => {
     // onPlayAgain(); // 게임 리셋되는 함수
     onClose();
@@ -10,10 +10,20 @@ const WordModalContext = ({ onClose }) => {
 
   return (
     <>
-      <h3>Challenge Completed!</h3>
-      <h4>Typing Callenge 게임을 다시 하시겠습니까?</h4>
-      <button onClick={playAgainHandler}>네! 또 할래요!</button>
-      <button onClick={onClose}>아니오. 안 할래요...</button>
+      <div  >
+        <h3 className='my-10 text-7xl text-center'>Challenge Completed!</h3>
+        <h4 className='my-16 text-4xl text-center'>Typing Challenge 게임을 다시 하시겠습니까?</h4>
+      </div>
+
+      <div className='flex justify-center'>
+        <button
+          className="m-8 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-4 px-10 border border-blue-500 hover:border-transparent rounded"
+          onClick={playAgainHandler}>네! 또 할래요!</button>
+        <button
+          className="m-8 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-4 px-10 border border-blue-500 hover:border-transparent rounded"
+          onClick={onClose}>아니오. 안 할래요...</button>
+      </div>
+
     </>
   )
 };
--- src/compoents/ui/Modal.jsx
+++ src/compoents/ui/Modal.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Modal = ({ children }) => {
+const Modal = ({ children, onClose }) => {
   return (
     <>
-      <div data-cy="modal-backdrop" className='fixed top-0 left-0 w-full h-full backdrop-blur-md z-1'
+      <div data-cy="modal-backdrop" className='fixed top-0 left-0 w-full h-full backdrop-blur-sm z-1'
         onClick={onClose}>
 
       </div>
-      <div>
+      <div
+        className='fixed z-10 w-1/2 h-3/5 p-8 m-0 transform -translate-x-1/2 -translate-y-1/2 border-none rounded shadow-xl top-1/2 left-1/2 bg-cyan-100'
+        >
         {children}
       </div>
 
