7c7cc84d066683615ab8346fd2849799d5f4be4f
juchan <rlawncks125@naver.com>
2023-12-14 10:16:04 +0900

    context

---
+++ src/contexts/WordContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useContext, useReducer } from "react";
+import { DEMY_WORD, PLAY_WORD_NUMBER } from "../constants/word";
+import React from "react";
+import { shuffleByArray } from "../utils/array";
+
+const WordContext = createContext();
+const WordDisPathContext = createContext();
+
+const reducerWord = (state, action) => {
+  switch (action.type) {
+    case "RANDDOM_WORD": {
+      const copy = state.originalData;
+      const playWord = shuffleByArray(copy);
+
+      return {
+        ...state,
+        playWord: playWord.splice(0, state.playWordNumber),
+      };
+    }
+    case "CORRECT": {
+      if (state.correctCount >= state.playWordNumber) {
+        return {
+          ...state,
+        };
+      }
+      const correctId = action.correctId;
+      state.playWord[correctId] = "";
+
+      const correctCount = state.correctCount + 1;
+
+      return {
+        ...state,
+        correctCount,
+        isPlaying: correctCount === state.playWordNumber ? false : true,
+      };
+    }
+
+    default:
+      break;
+  }
+};
+
+const WordProvider = ({ children }) => {
+  const [words, wordDispath] = useReducer(reducerWord, {
+    originalData: DEMY_WORD,
+    playWord: [],
+    playWordNumber: PLAY_WORD_NUMBER,
+    correctCount: 0,
+    isPlaying: true,
+  });
+
+  return (
+    <WordContext.Provider value={words}>
+      <WordDisPathContext.Provider value={wordDispath}>
+        {children}
+        <div className="border-2n"></div>
+      </WordDisPathContext.Provider>
+    </WordContext.Provider>
+  );
+};
+
+export default WordProvider;
+export const useWord = () => useContext(WordContext);
+export const useWordDispath = () => useContext(WordDisPathContext);
